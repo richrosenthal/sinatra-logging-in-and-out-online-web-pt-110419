3215336728da861a010758344f68ba42df82cbb4
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-03-15 18:28:33 +0000

    Automatically backed up by Learn

--- db/migrate/20200315182720_users.rb
+++ db/migrate/20200315182720_users.rb
@@ -2,7 +2,7 @@ class Users < ActiveRecord::Migration[5.1]
   def change
       create_table :users do |t|
       t.string :username
-      t.string :email
       t.string :password
+      t.integer :balance
   end
 end
